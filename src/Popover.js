a7d6ca5604c6023f5c84d275ef745f6724b23f37
Hikurangi Schaverien-Kaa <hikurangi@gmail.com>
2020-02-04 16:11:51 +1300

    add border to active avatar in popup

--- src/Popover.js
+++ src/Popover.js
@@ -20,11 +20,15 @@ const Popover = ({
     })
   }
 
+  function getIsActiveAvatar (avatar) {
+    return avatar.id === current.id
+  }
+
   const popoverRef = useRef(null)
   useOutsideClick(popoverRef)
 
   // remove the current avatar
-  const existingAvatars = choices.filter(choice => choice.id !== current.id)
+  const existingAvatars = choices.filter(choice => !getIsActiveAvatar(choice))
 
   // keep the current avatar at the start
   const avatars = [current, ...existingAvatars]
@@ -33,15 +37,19 @@ const Popover = ({
     <div className="popover" ref={popoverRef}>
       <p className="popover-title">Choose your avatar</p>
       <section className="avatar-wrapper">
-        {avatars.map(avatar => (
-          <img
-            key={avatar.id}
-            className="avatar"
-            onClick={() => selectAvatar(avatar)}
-            src={avatar.src}
-            alt={avatar.alt}
-          />
-        ))}
+        {avatars.map(avatar => {
+          const className = getIsActiveAvatar(avatar) ? "avatar active" : "avatar"
+          
+          return (
+            <img
+              key={avatar.id}
+              className={className}
+              onClick={() => selectAvatar(avatar)}
+              src={avatar.src}
+              alt={avatar.alt}
+            />
+          )
+        })}
       </section>
     </div>
   )
